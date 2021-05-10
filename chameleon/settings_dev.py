8ccd8d6e2eaa9c54a02adcaf8b71ac9527e2f482
KAN <private@mars.com>
2021-05-10 23:56:55 +0800

    upload settings_dev

---
+++ chameleon/settings_dev.py
@@ -0,0 +1,13 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chameleon_db_dev',
+        'USER': 'root',
+        'PASSWORD': 'chameleon',
+        'HOST': '159.75.82.228',
+        'PORT': '3306',
+    }
+}
