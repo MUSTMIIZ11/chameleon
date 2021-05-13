4948e474813d0ba4d76f51f55c9ed458ccd22769
lulindeng <65015333+lulindeng@users.noreply.github.com>
2021-05-13 21:09:13 +0800

    update community backends

--- community/models.py
+++ community/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Map(models.Model):
-    map_id = models.IntegerField()
+    # map_id = models.IntegerField()
     map_name = models.CharField(max_length=50)
     map_url = models.TextField(max_length=100)
     user_id = models.IntegerField()
-    create_time = models.DateField()
+    like = models.IntegerField(default=0)
+    create_time = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.map_name
--- community/views.py
+++ community/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Map
 
 
 def index(request):
-    return render(request, 'community.html')
+    ordered_map = Map.objects.order_by('-like').all()[:9]
+    display_map_list = dict()
+    for i in range(9):
+        display_map_list['map' + str(i)] = ordered_map[i].map_url
+    print(display_map_list)
+    return render(request, 'community.html', display_map_list)
