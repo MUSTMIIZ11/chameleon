da985192da4e3b49cee59c1b1c8e36821613493b
KAN <private@mars.com>
2021-05-15 13:46:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,12 @@ IMAGE="$CONTAINER":latest
 # 创建新镜像
 docker build -t $IMAGE . && \
 # delete running images
+
 RUN_NAME="$CONTAINER""service"
-docker rm -f `docker ps -a | grep -w RUN_NAME | awk '{print $1}'`
+if docker ps -a | grep -w \"$RUN_NAME\"
+then
+  docker rm -f $RUN_NAME
+fi
 # run new images
 docker run --name RUN_NAME -d "$CONTAINER"
 echo "Run successfully"
