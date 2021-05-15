e15bb29719cee5e13a2d90144ba53c4ab51c066f
KAN <private@mars.com>
2021-05-15 12:50:08 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 容器名称
+CONTAINER="chameleon"
+IMAGE="$CONTAINER":latest
+# 创建新镜像
+docker build -t $IMAGE . && \
+# delete running images
+RUN_NAME="$CONTAINER""service"
+docker rm -f `docker ps -a | grep -w RUN_NAME | awk '{print $1}'`
+# run new images
+docker run --name RUN_NAME -d "$CONTAINER"
