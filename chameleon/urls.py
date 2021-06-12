d6958fa09d9525f932995aad8e29a708f6cc5a8a
lulindeng <65015333+lulindeng@users.noreply.github.com>
2021-06-12 14:11:01 +0800

    Update urls.py

--- chameleon/urls.py
+++ chameleon/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 from chameleon import settings
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'),name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('welcome/', include('welcome.urls')),
     path('community/', include('community.urls')),
     path('tool/',include('tools.urls')),
